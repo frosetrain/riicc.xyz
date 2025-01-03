9285abaa6612e041efbe4574b1e34a330100bb45
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2025-01-03 17:17:10 +0800

    Add under construction page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="mx-auto max-w-2xl lg:max-w-7xl p-2">
+    <div class="rounded-3xl bg-gray-300 p-4">
+        <h1 class="text-center font-bold text-5xl">Under Construction</h1>
+    </div>
+    <img class="mt-8 mx-auto" src="/riicc-onlight.svg" />
+    <p class="text-center mt-4 font-medium text-lg">Raffles Institution Infocomm Club</p>
+    <div class="mt-8 p-4 border w-fit mx-auto rounded-md">
+        <div class="flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z"
+                />
+            </svg>
+            <div>
+                <p class="font-medium text-lg">There’s nothing here…</p>
+                <p>We will be building this website sometime soon.</p>
+            </div>
+        </div>
+    </div>
+</div>
