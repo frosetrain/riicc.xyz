70f53c90ce76d83cad4f4bdb4b51737046f76e68
Frosetrain <80436189+frosetrain@users.noreply.github.com>
2025-01-06 20:19:53 +0800

    Add project showcase

---
+++ src/routes/shackathon/+page.svelte
@@ -0,0 +1,22 @@
+<div class="m-4">
+    <h1 class="font-bold text-4xl mb-4">RIICC Project Showcase</h1>
+
+    <h2 class="font-bold text-2xl">Cratch Shackathon</h2>
+    <p>A goofy ahh cca event where teams had 2 hours to make something</p>
+    <p>Ranked in order of how good they are</p>
+    <ol class="list-decimal list-inside">
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094511309">https://scratch.mit.edu/projects/1094511309</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094536902">https://scratch.mit.edu/projects/1094536902</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094509112">https://scratch.mit.edu/projects/1094509112</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094519855">https://scratch.mit.edu/projects/1094519855</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094509801">https://scratch.mit.edu/projects/1094509801</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094515028">https://scratch.mit.edu/projects/1094515028</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094510927">https://scratch.mit.edu/projects/1094510927</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094514259">https://scratch.mit.edu/projects/1094514259</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094520311">https://scratch.mit.edu/projects/1094520311</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094513388">https://scratch.mit.edu/projects/1094513388</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094510608">https://scratch.mit.edu/projects/1094510608</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094510211">https://scratch.mit.edu/projects/1094510211</a></li>
+        <li><a class="text-blue-700 underline" href="https://scratch.mit.edu/projects/1094516100">https://scratch.mit.edu/projects/1094516100</a></li>
+    </ol>
+</div>
